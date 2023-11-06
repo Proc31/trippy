73e0330138989de6323ff2979c5b7a0919a35916
CoupDeWhoop <greenland.david@gmail.com>
2023-11-06 16:49:26 +0000

    added basic inventory function

--- src/components/UserIndex.tsx
+++ src/components/UserIndex.tsx
@@ -1,15 +1,18 @@
 import { useState } from 'react';
 import { BottomNavigation, Text } from 'react-native-paper';
 import Generator from './Generator';
+import InventoryScreen from './InventoryScreen';
 
 export default function UserIndex() {
 	const [index, setIndex] = useState(0);
 	const [routes] = useState([
 		{ key: 'generator', title: 'Generator', icon: 'icon' },
+		{ key: 'inventory', title: 'Inventory', icon: 'icon' },
 	]);
 
 	const renderScene = BottomNavigation.SceneMap({
 		generator: Generator,
+		inventory: InventoryScreen,
 	});
 
 	return (
