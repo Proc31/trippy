ac38e6cccf0a3098a712701c067908e99f0c0e8c
joe <skelbon@gmail.com>
2023-11-07 10:52:00 +0000

    added dot env for fb cofig tokens etc

--- firebase/db/config.jsx
+++ firebase/db/config.jsx
@@ -1,22 +1,35 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth"
+import {
+	FB_API_KEY,
+	FB_AUTH_DOMAIN,
+	FB_DB_URL, 
+	FB_PROJECT_ID,
+	FB_STORAGE_BUCKET, 
+	FB_MESSAGING_SENDER_ID, 
+	FB_APP_ID, 
+	FB_MEASUREMENT_ID } 
+	from '@env';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDvEWdGQw0s-I28ZDDw4w-Yp2lX4pl58lE",
-  authDomain: "trippy-fad48.firebaseapp.com",
-  databaseURL: "https://trippy-fad48-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "trippy-fad48",
-  storageBucket: "trippy-fad48.appspot.com",
-  messagingSenderId: "799332098976",
-  appId: "1:799332098976:web:61b7d7818f6d789f95bac5",
-  measurementId: "G-EDFPJQB6Z3"
+  apiKey: FB_API_KEY,
+  authDomain: FB_AUTH_DOMAIN,
+  databaseURL: FB_DB_URL, 
+  projectId: FB_PROJECT_ID,
+  storageBucket: FB_STORAGE_BUCKET, 
+  messagingSenderId: FB_MESSAGING_SENDER_ID, 
+  appId: Fb_APP_ID, 
+  measurementId: FB_MEASUREMENT_ID 
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const analytics = getAnalytics(app);
+export const FIREBASE_AUTH = getAuth(app);
--- src/Home.tsx
+++ src/Home.tsx
@@ -1,29 +1,29 @@
-import * as React from 'react';
-import { View, Button } from 'react-native';
-import { AuthContext } from './Contexts';
+import * as React from "react";
+import { View, Button } from "react-native";
+import { AuthContext } from "./Contexts";
 
 export default function Home({ navigation }) {
-	const { signOut } = React.useContext(AuthContext);
+  const { signOut } = React.useContext(AuthContext);
 
-	return (
-		<>
-			<View>
-				<Button title="Sign out" onPress={signOut} />
-			</View>
-			<View
-				style={{
-					flex: 1,
-					alignItems: 'center',
-					justifyContent: 'center',
-				}}
-			>
-				<Button
-					title="Move to user index section"
-					onPress={() => navigation.navigate('UserIndex')}
-				/>
-			</View>
-		</>
-	);
+  return (
+    <>
+      <View>
+        <Button title="Sign out" onPress={signOut} />
+      </View>
+      <View
+        style={{
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Button
+          title="Move to user index section"
+          onPress={() => navigation.navigate("UserIndex")}
+        />
+      </View>
+    </>
+  );
 }
 
 //Page to display after the user has logged in!
