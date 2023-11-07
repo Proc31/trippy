3c808907b784a6b2cbed42a8b77c63a8d6c5b6a2
joe <skelbon@gmail.com>
2023-11-07 12:26:08 +0000

    added MVP auth email/password

--- src/SignIn.tsx
+++ src/SignIn.tsx
@@ -1,26 +1,27 @@
 import * as React from "react";
 import { Button, TextInput, View } from "react-native";
 import { AuthContext } from "./Contexts";
+import { FIREBASE_AUTH } from "../firebase/config";
 
 export default function SignIn() {
-  const [username, setUsername] = React.useState("");
+  const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
   const { signIn } = React.useContext(AuthContext);
+  const auth = FIREBASE_AUTH;
 
   return (
     <View>
-      <TextInput
-        placeholder="Username"
-        value={username}
-        onChangeText={setUsername}
-      />
+      <TextInput placeholder="Email" value={email} onChangeText={setEmail} />
       <TextInput
         placeholder="Password"
         value={password}
         onChangeText={setPassword}
         secureTextEntry
       />
-      <Button title="Sign in" onPress={() => signIn({ username, password })} />
+      <Button
+        title="Sign in"
+        onPress={() => signIn({ email, password, auth })}
+      />
     </View>
   );
 }
