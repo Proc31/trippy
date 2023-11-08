a82c481065169be2f078a79068e57f3b48ae1b6a
Matthew Proctor <matt@Matts-Mac.local>
2023-11-08 12:29:39 +0000

    refactor functions so they work

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -2,71 +2,59 @@ import * as Firebase from 'firebase/database';
 
 const db = Firebase.getDatabase();
 
-export function getGuardians() {
+export async function getGuardians() {
 	const ref = Firebase.ref(db, 'guardians');
-	Firebase.onValue(ref, (snapshot) => {
-		const data = snapshot.val();
-		const keys = Object.keys(data);
-		const result = keys.map((guardian) => {
-			return { [guardian]: data[guardian] };
-		});
-		return result;
+	const data = await Firebase.get(ref);
+	const guardians = data.val();
+	const keys = Object.keys(guardians);
+	const result = keys.map((guardian) => {
+		return { [guardian]: guardians[guardian] };
 	});
+	return result;
 }
 
-export function getStudents() {
+export async function getStudents() {
 	const ref = Firebase.ref(db, 'students');
-	Firebase.onValue(ref, (snapshot) => {
-		const data = snapshot.val();
-		const keys = Object.keys(data);
-		const result = keys.map((student) => {
-			return { [student]: data[student] };
-		});
-		return result;
+	const data = await Firebase.get(ref);
+	const students = data.val();
+	const keys = Object.keys(students);
+	const result = keys.map((student) => {
+		return { [student]: students[student] };
 	});
+	return result;
 }
 
-export function getTrips() {
+export async function getTrips() {
 	const ref = Firebase.ref(db, 'trips');
-	Firebase.onValue(ref, (snapshot) => {
-		const data = snapshot.val();
-		const keys = Object.keys(data);
-		const result = keys.map((trip) => {
-			return { [trip]: data[trip] };
-		});
-		return result;
+	const data = await Firebase.get(ref);
+	const trips = data.val();
+	const keys = Object.keys(trips);
+	const result = keys.map((trip) => {
+		return { [trip]: trips[trip] };
 	});
-}
-
-
-
-export function getSingleStudent(id) {
-	const ref = Firebase.ref(db, `students/${id}`);
-	Firebase.onValue(ref, (snapshot) => {
-		const result = snapshot.val();
-		return result;
-	});
-}
-
-export function getSingleTeacher(id) {
-	const ref = Firebase.ref(db, `teachers/${id}`);
-	Firebase.onValue(ref, (snapshot) => {
-		const result = snapshot.val();
-		return result;
-	});
-}
-
-export function getUserRole(id) {
-	const ref = Firebase.ref(db, `users/${id}`);
-	Firebase.onValue(ref, (snapshot) => {
-		const data = snapshot.val();
-		const result = data.role;
-		return result;
-	});
+	return result;
 }
 
 export async function getSingleTrip(id) {
 	const ref = Firebase.ref(db, `trips/${id}`);
+	const result = await Firebase.get(ref);
+	return result;
+}
+
+export async function getSingleStudent(id) {
+	const ref = Firebase.ref(db, `students/${id}`);
+	const result = await Firebase.get(ref);
+	return result;
+}
+
+export async function getSingleTeacher(id) {
+	const ref = Firebase.ref(db, `teachers/${id}`);
+	const result = await Firebase.get(ref);
+	return result;
+}
+
+export async function getUserRole(id) {
+	const ref = Firebase.ref(db, `users/${id}`);
 	const result = await Firebase.get(ref);
 	return result;
 }
