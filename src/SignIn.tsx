f61fd5984bfa5e99ffd65d673b5c36c5e01a26c5
Matthew Proctor <matt@Matts-Mac.local>
2023-11-08 10:39:11 +0000

    implemented getTrips

--- src/SignIn.tsx
+++ src/SignIn.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import { Button, TextInput, View } from "react-native";
 import { AuthContext } from "./Contexts";
 import { FIREBASE_AUTH } from "../firebase/config";
-import { getGuardians } from './utils/utils';
 
 export default function SignIn() {
 	const [email, setEmail] = React.useState('');
@@ -27,6 +26,7 @@ export default function SignIn() {
 				title="Sign in"
 				onPress={() => signIn({ email, password, auth })}
 			/>
+
 		</View>
 	);
 }
