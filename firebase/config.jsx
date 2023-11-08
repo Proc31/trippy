44870a2f40d8c862db4e65125703cddb39fa516f
joe <skelbon@gmail.com>
2023-11-08 18:00:30 +0000

    put ananlytics back in firebase config file - causes error without1?!

--- firebase/config.jsx
+++ firebase/config.jsx
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import { getAnalytics } from "firebase/analytics";
 
 import {
 	FB_API_KEY,
@@ -32,4 +33,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 export const FIREBASE_AUTH = getAuth(app);
