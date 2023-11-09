10cd244cad6872b16cbc464a97d95a657211de4e
joe <skelbon@gmail.com>
2023-11-09 14:56:45 +0000

    not getting rerender with user update

--- firebase/auth/AuthContext.tsx
+++ firebase/auth/AuthContext.tsx
@@ -2,13 +2,15 @@ import * as React from "react";
 
 import { createContext, useContext, useState } from "react";
 
-import { User, signInWithEmailAndPassword } from "firebase/auth";
+import { User, signInWithEmailAndPassword, signOut } from "firebase/auth";
 
 import { FIREBASE_AUTH } from "firebase/config";
+import { Text } from "react-native";
 
 interface AuthContextData {
   user: User | null;
   isLoading: boolean;
+  isSignOut: boolean;
   login(username: string, password: string): void;
   logout(): void;
 }
@@ -18,27 +20,32 @@ const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 const AuthProvider: React.FC = ({ children }) => {
   const [user, setUser] = useState<User | null>(null);
   const [isLoading, setLoading] = useState<boolean>(true);
+  const [isSignOut, setSignOut] = useState<boolean>(false);
 
   async function login(email: string, password: string) {
-    const userCredential = await signInWithEmailAndPassword(
-      FIREBASE_AUTH,
-      email,
-      password
-    );
-    setUser(userCredential.user);
-    console.log(user);
-
-    setLoading(false);
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+        FIREBASE_AUTH,
+        email,
+        password
+      );
+      setUser(userCredential.user);
+      setSignOut(false);
+      setLoading(false);
+      console.log(user);
+    } catch (err) {
+      console.log(err);
+    }
   }
 
-  function logout() {
-    setUser((prevState) => {
-      return null;
-    });
+  async function logout() {
+    await FIREBASE_AUTH.signOut();
+    setUser(null);
+    setSignOut(true);
   }
 
   return (
-    <AuthContext.Provider value={{ user, isLoading, login, logout }}>
+    <AuthContext.Provider value={{ user, isLoading, isSignOut, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
--- src/Index.tsx
+++ src/Index.tsx
@@ -4,22 +4,29 @@ import { useAuth, AuthProvider } from "../firebase/auth/AuthContext";
 import Home from "./Home";
 import SignIn from "./SignIn";
 import UserIndex from "./components/UserIndex";
+import SplashScreen from "./SplashSreen";
+import { Alert, Button } from "react-native";
 
 const Stack = createNativeStackNavigator();
 
 export default function Index() {
-  const user = useAuth;
+  const { user, isSignOut, isLoading } = useAuth();
 
   return (
     <AuthProvider>
       <Stack.Navigator>
-        {!user ? (
-          // User isn't signed in
+        {isLoading ? (
+          // We haven't finished checking for the token yet
+          <Stack.Screen name="Splash" component={SplashScreen} />
+        ) : user == null ? (
+          // No token found, user isn't signed in
           <Stack.Screen
             name="SignIn"
             component={SignIn}
             options={{
               title: "Sign in",
+              // When logging out, a pop animation feels intuitive
+              animationTypeForReplace: isSignOut ? "pop" : "push",
             }}
           />
         ) : (
