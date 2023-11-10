06fb64cacb8fa02922b83b244d6e435e7cbf8752
Matthew Proctor <matt@Matts-Mac.local>
2023-11-10 16:43:04 +0000

    removed splash screen and replaced with loading

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
 import { ActivityIndicator } from 'react-native-paper';
 import { View } from 'react-native';
+import theme from './ux/Theme';
 
 export default function Loading() {
 	return (
 		<View
 			style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
 		>
-			<ActivityIndicator animating={true} size="large" />
+			<ActivityIndicator animating={true} size="large" theme={theme} />
 		</View>
 	);
 }
