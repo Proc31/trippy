0089e8f7dc6127127686b4cb15c4db6e262347cf
skelbon <119545226+skelbon@users.noreply.github.com>
2023-11-10 09:16:57 +0000

    Revert "Revert "Auth""

---
+++ firebase/auth/AuthContext.tsx
@@ -0,0 +1,65 @@
+import * as React from "react";
+
+import { createContext, useContext, useState } from "react";
+
+import { User, signInWithEmailAndPassword, signOut } from "firebase/auth";
+
+import { FIREBASE_AUTH } from "firebase/config";
+
+interface AuthContextData {
+  user: User | null;
+  isLoading: boolean;
+  isSignOut: boolean;
+  login(username: string, password: string): void;
+  logout(): void;
+}
+
+const AuthContext = createContext<AuthContextData>({} as AuthContextData);
+
+const AuthProvider: React.FC = ({
+  children,
+}: {
+  children?: React.FC | React.FC[];
+}) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [isLoading, setLoading] = useState<boolean>(true);
+  const [isSignOut, setSignOut] = useState<boolean>(false);
+
+  async function login(email: string, password: string) {
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+        FIREBASE_AUTH,
+        email,
+        password
+      );
+      setUser(userCredential.user);
+      setLoading(false);
+    } catch (err) {
+      console.log("Got an err");
+    }
+    return user;
+  }
+
+  function logout() {
+    signOut(FIREBASE_AUTH);
+    setUser(null);
+    setSignOut(true);
+  }
+
+  return (
+    <AuthContext.Provider value={{ user, isLoading, isSignOut, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+function useAuth(): AuthContextData {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error("useAuth needs an AuthProvider");
+  }
+  return context;
+}
+
+export { AuthProvider, useAuth };
--- src/Index.tsx
+++ src/Index.tsx
@@ -1,126 +1,35 @@
 import * as React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { AuthContext } from "./Contexts";
+import { useAuth, AuthProvider } from "../firebase/auth/AuthContext";
 import Home from "./Home";
 import SignIn from "./SignIn";
-import SplashScreen from "./SplashSreen";
 import UserIndex from "./components/UserIndex";
-
-
-import { signInWithEmailAndPassword } from "firebase/auth";
 
 const Stack = createNativeStackNavigator();
 
 export default function Index() {
-  const [state, dispatch] = React.useReducer(
-    (prevState, action) => {
-      switch (action.type) {
-        case "RESTORE_TOKEN":
-          return {
-            ...prevState,
-            userToken: action.token,
-            isLoading: false,
-          };
-        case "SIGN_IN":
-          return {
-            ...prevState,
-            isSignout: false,
-            userToken: action.token,
-          };
-        case "SIGN_OUT":
-          return {
-            ...prevState,
-            isSignout: true,
-            userToken: null,
-          };
-      }
-    },
-    {
-      isLoading: true,
-      isSignout: false,
-      userToken: null,
-    }
-  );
+  const { user, isSignOut, isLoading } = useAuth();
 
-  React.useEffect(() => {
-    // Fetch the token from storage then navigate to our appropriate place
-    const bootstrapAsync = async () => {
-      let userToken;
-
-      try {
-        // Restore token stored in `SecureStore` or any other encrypted storage
-        // userToken = await SecureStore.getItemAsync('userToken');
-      } catch (e) {
-        // Restoring token failed
-      }
-
-      // After restoring token, we may need to validate it in production apps
-      // This will switch to the App screen or Auth screen and this loading
-      // screen will be unmounted and thrown away.
-      dispatch({ type: "RESTORE_TOKEN", token: userToken });
-    };
-
-    bootstrapAsync();
-  }, []);
-
-  const authContext = React.useMemo(
-    () => ({
-      signIn: async ({ email, password, auth }) => {
-        try {
-          const response = await signInWithEmailAndPassword(
-            auth,
-            email,
-            password
-          );
-          dispatch({ type: "SIGN_IN", token: response });
-        } catch (error: any) {
-          console.log(error);
-          alert("Sign in failed:" + error.message);
-        }
-
-        // In a production app, we need to send some data (usually username, password) to server and get a token
-        // We will also need to handle errors if sign in failed
-        // After getting token, we need to persist the token using `SecureStore` or any other encrypted storage
-        // In the example, we'll use a dummy token
-      },
-      signOut: () => dispatch({ type: "SIGN_OUT" }),
-      signUp: async (data) => {
-        // In a production app, we need to send user data to server and get a token
-        // We will also need to handle errors if sign up failed
-        // After getting token, we need to persist the token using `SecureStore` or any other encrypted storage
-        // In the example, we'll use a dummy token
-
-        dispatch({ type: "SIGN_IN", token: "dummy-auth-token" });
-      },
-    }),
-    []
-  );
   return (
-    <AuthContext.Provider value={authContext}>
-      <Stack.Navigator>
-        {state.isLoading ? (
-          // We haven't finished checking for the token yet
-          <Stack.Screen name="Splash" component={SplashScreen} />
-        ) : state.userToken == null ? (
-          // No token found, user isn't signed in
-          <Stack.Screen
-            name="SignIn"
-            component={SignIn}
-            options={{
-              title: "Sign in",
-              // When logging out, a pop animation feels intuitive
-              animationTypeForReplace: state.isSignout ? "pop" : "push",
-            }}
-          />
-        ) : (
-          // User is signed in
-          <>
-            <Stack.Screen name="Home" component={Home} />
+    <Stack.Navigator>
+      {!user ? (
+        <Stack.Screen
+          name="SignIn"
+          component={SignIn}
+          options={{
+            title: "Sign in",
+            animationTypeForReplace: isSignOut ? "pop" : "push",
+          }}
+        />
+      ) : (
+        // User is signed in
 
-            <Stack.Screen name="UserIndex" component={UserIndex} />
-          </>
-        )}
-      </Stack.Navigator>
-    </AuthContext.Provider>
+        <>
+          <Stack.Screen name="Home" component={Home} />
+
+          <Stack.Screen name="UserIndex" component={UserIndex} />
+        </>
+      )}
+    </Stack.Navigator>
   );
 }
