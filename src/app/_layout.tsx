0bec3feff8a7c2d8be07b060c18793b403599164
harryRobinson <127695266+Hazbob@users.noreply.github.com>
2023-11-14 11:21:50 +0000

    added theme for lists and can be used from the theme section

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,20 +1,39 @@
-import * as React from 'react';
-import { Slot } from 'expo-router';
-import { SessionProvider } from '../auth/ctx';
-import { PaperProvider } from 'react-native-paper';
-import { LogBox } from 'react-native';
-import theme from '@/utils/theme';
+import * as React from "react";
+import { Slot } from "expo-router";
+import { SessionProvider } from "../auth/ctx";
+import { PaperProvider } from "react-native-paper";
+import { LogBox } from "react-native";
+import theme from "@/utils/theme";
+import { Text, View } from "react-native";
+import { SplashScreen } from "expo-router";
+import { useFonts } from "expo-font";
 
-LogBox.ignoreLogs(['Warning: ...']); // Ignore log notification by message
+SplashScreen.preventAutoHideAsync();
+
+LogBox.ignoreLogs(["Warning: ..."]); // Ignore log notification by message
 LogBox.ignoreAllLogs(); //Ignore all log notifications
 
 export default function Root() {
-	// Set up the auth context and render our layout inside of it.
-	return (
-		<SessionProvider>
-			<PaperProvider theme={theme}>
-				<Slot />
-			</PaperProvider>
-		</SessionProvider>
-	);
+  const [fontsLoaded, fontError] = useFonts({
+    "Poetsen One": require("../assets/fonts/PoetsenOne-Regular.ttf"),
+  });
+  React.useEffect(() => {
+    if (fontsLoaded || fontError) {
+      // Hide the splash screen after the fonts have loaded (or an error was returned) and the UI is ready.
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  // Prevent rendering until the font has loaded or an error was returned
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
+  return (
+    <SessionProvider>
+      <PaperProvider theme={theme}>
+        <Slot />
+      </PaperProvider>
+    </SessionProvider>
+  );
 }
--- src/components/SORT THESE/StudentList.tsx
+++ src/components/SORT THESE/StudentList.tsx
@@ -1,9 +1,7 @@
 import React, { useState, useCallback } from "react";
 import { StyleSheet, Text, View, Button, ScrollView } from "react-native";
 import { Checkbox } from "react-native-paper";
-import { useFonts } from "expo-font";
-import * as SplashScreen from "expo-splash-screen";
-SplashScreen.preventAutoHideAsync();
+import theme from "../../utils/theme";
 
 export default function StudentList({
   title,
@@ -11,21 +9,7 @@ export default function StudentList({
   setCheckedItems,
   students,
 }) {
-  const [fontsLoaded] = useFonts({
-    "Super Summer": require("../../assets/fonts/SuperSummer.ttf"),
-    Handmade: require("../../assets/fonts/Handmade.otf"),
-  });
-  const onLayoutRootView = useCallback(async () => {
-    if (fontsLoaded) {
-      await SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded]);
-
-  if (!fontsLoaded) {
-    return null;
-  }
   const handleCheckboxChange = (id) => {
-    //this gets passed the student id
     const currentIndex = checkedItems.indexOf(id);
     const newCheckedItems = [...checkedItems];
 
@@ -39,32 +23,18 @@ export default function StudentList({
   };
   const studentsTest = students.map((student) => {});
   return (
-    <ScrollView style={styles.container}>
+    <ScrollView style={theme.listContainer}>
       {title}
       {students.map((student, index) => (
-        <View
-          key={index + 43}
-          style={styles.checkboxContainer}
-          onLayout={onLayoutRootView}
-        >
+        <View key={index + 43} style={theme.checkboxContainer}>
           <Checkbox
+            uncheckedColor="#2a2c41"
+            color="white"
             key={index + 100}
             status={checkedItems.includes(student.id) ? "checked" : "unchecked"}
             onPress={() => handleCheckboxChange(student.id)}
           />
-          <Text
-            key={index + 299}
-            style={{
-              fontFamily: "Handmade",
-              marginLeft: 8,
-              fontSize: 32,
-              borderStyle: "solid",
-              backgroundColor: "#73B5D4",
-              height: "100%",
-              width: "100%",
-              padding: 5,
-            }}
-          >
+          <Text key={index + 299} style={theme.listText}>
             {student.first_name + " " + student.surname}
           </Text>
         </View>
@@ -72,20 +42,3 @@ export default function StudentList({
     </ScrollView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginHorizontal: 16,
-    marginVertical: 32,
-  },
-  checkboxContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginBottom: 12,
-  },
-  label: {
-    marginLeft: 8,
-    fontSize: 16,
-  },
-});
