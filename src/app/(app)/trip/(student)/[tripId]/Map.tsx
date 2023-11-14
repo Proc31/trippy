27ee89d9ae7baaeea59952ea7a0a4a24973a386b
Matthew Proctor <matt@Matts-Mac.local>
2023-11-14 18:22:35 +0000

    student map local version implemented

--- src/app/(app)/trip/(student)/[tripId]/Map.tsx
+++ src/app/(app)/trip/(student)/[tripId]/Map.tsx
@@ -1,31 +1,54 @@
 import * as React from 'react';
-import { View, StyleSheet, Dimensions } from 'react-native';
-import { Text } from 'react-native-paper';
-import MapView, { Marker } from 'react-native-maps';
 import * as Location from 'expo-location';
+import { View, StyleSheet } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 import Loading from '@/components/global/Loading';
+import {
+	FAB,
+	Modal,
+	Portal,
+	List,
+	Button,
+	IconButton,
+} from 'react-native-paper';
+import { ScrollView } from 'react-native-gesture-handler';
 
-const { width, height } = Dimensions.get('window');
+const LATITUDE_DELTA = 0;
+const LONGITUDE_DELTA = 0.01;
+const CAMERA_SPEED = 3000;
 
-const SCREEN_HEIGHT = height;
-const SCREEN_WIDTH = width;
-const ASPECT_RATIO = SCREEN_WIDTH / SCREEN_HEIGHT;
-const LATITUDE_DELTA = 0.0922;
-const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+const containerStyle = { backgroundColor: 'white', padding: 20 };
 
 export default function Map() {
 	const [location, setLocation] = React.useState(null);
 	const [errorMsg, setErrorMsg] = React.useState(null);
 	const [isLoading, setIsLoading] = React.useState(true);
-	const [students, setStudents] = React.useState([
+	const [visible, setVisible] = React.useState(false);
+	const [markers, setMarkers] = React.useState([
 		{
-			name: 'Meet Up Point',
-			location: {
+			id: 'testid1',
+			title: 'Meet Up Point',
+			description: 'Please be nice and meet your teacher.',
+			coordinate: {
 				latitude: 53.47216811021656,
 				longitude: -2.2382618858453327,
 			},
 		},
+		{
+			id: 'testid2',
+			title: `Don't go here`,
+			description: `Pleaes don't.`,
+			coordinate: {
+				latitude: 39.0738,
+				longitude: 125.8198,
+			},
+		},
 	]);
+
+	const mapRef = React.useRef(null);
+
+	const showModal = () => setVisible(true);
+	const hideModal = () => setVisible(false);
 
 	React.useEffect(() => {
 		(async () => {
@@ -54,16 +77,75 @@ export default function Map() {
 
 	return (
 		<View style={styles.container}>
-			<MapView style={styles.map} region={location} showsUserLocation>
-				{students.map((student) => {
+			<MapView
+				ref={mapRef}
+				style={styles.map}
+				region={location}
+				showsUserLocation
+			>
+				{markers.map((marker) => {
 					return (
 						<Marker
-							title={student.name}
-							coordinate={student.location}
+							key={marker.id}
+							title={marker.title}
+							coordinate={marker.coordinate}
+							description={marker.description}
 						></Marker>
 					);
 				})}
 			</MapView>
+			<FAB
+				icon="map-marker"
+				style={styles.fab}
+				onPress={showModal}
+				mode="elevated"
+				size="small"
+				theme={{
+					colors: {
+						primaryContainer: 'white',
+						onPrimaryContainer: 'green',
+					},
+				}}
+			/>
+			<Portal>
+				<Modal
+					visible={visible}
+					onDismiss={hideModal}
+					contentContainerStyle={containerStyle}
+				>
+					<View>
+						<ScrollView>
+							{markers.map((marker) => {
+								return (
+									<List.Item
+										key={marker.id}
+										title={marker.title}
+										description={marker.description}
+										right={() => (
+											<IconButton
+												icon="map-marker"
+												size={32}
+												iconColor="green"
+												onPress={() => {
+													hideModal();
+													mapRef.current.animateCamera(
+														{
+															center: marker.coordinate,
+															zoom: 17,
+														},
+														CAMERA_SPEED
+													);
+												}}
+											/>
+										)}
+									></List.Item>
+								);
+							})}
+						</ScrollView>
+						<Button onPress={hideModal}>Close Window</Button>
+					</View>
+				</Modal>
+			</Portal>
 		</View>
 	);
 }
@@ -78,5 +160,12 @@ const styles = StyleSheet.create({
 	map: {
 		width: '100%',
 		height: '100%',
+	},
+	fab: {
+		position: 'absolute',
+		margin: 16,
+		right: 0,
+		bottom: 100,
+		zIndex: 10,
 	},
 });
