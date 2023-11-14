aa76ba903567d03ecfa63fcd45c58d7431e68d22
harryRobinson <127695266+Hazbob@users.noreply.github.com>
2023-11-14 15:43:24 +0000

    added theming for the trip cards

--- src/components/triplist/GuardianTripCard.tsx
+++ src/components/triplist/GuardianTripCard.tsx
@@ -1,51 +1,52 @@
-import * as React from 'react';
-import { Card, Button, Text, Title, Subheading } from 'react-native-paper';
-import InventoryScreen from '@/components/inventory/InventoryScreen';
-import { router } from 'expo-router';
+import * as React from "react";
+import { Card, Button, Text, Title, Subheading } from "react-native-paper";
+import InventoryScreen from "@/components/inventory/InventoryScreen";
+import { router } from "expo-router";
+import theme from "@/utils/theme";
 
 const GuardianTripCard = ({
-	tripId,
-	tripDetails,
-	handleEditTrip,
-	handleDeleteTrip,
-	userRole,
+  tripId,
+  tripDetails,
+  handleEditTrip,
+  handleDeleteTrip,
+  userRole,
 }) => {
-	return (
-		<Card style={{ margin: 16 }}>
-			<Card.Content>
-				<Title style={{ fontSize: 24, fontWeight: 'bold' }}>
-					{tripDetails.name}
-				</Title>
-				<Text variant="titleMedium">{tripDetails.date}</Text>
-				<Subheading style={{ color: 'blue' }}>
-					{tripDetails.location}
-				</Subheading>
-				<Text variant="bodyMedium">{tripDetails.description}</Text>
-			</Card.Content>
-			<Card.Cover
-				style={{ padding: 6 }}
-				source={{ uri: 'https://picsum.photos/700' }}
-			/>
-			<Card.Actions
-				style={{ display: 'flex', justifyContent: 'space-between' }}
-			>
-				<Button
-					style={{ marginRight: 'auto', flex: 1 }}
-					onPress={() =>
-						router.push({
-							pathname: `/trip/(${userRole})/[tripId]`,
-							params: {
-								tripId,
-								tripTitle: tripDetails.name,
-							},
-						})
-					}
-				>
-					Info
-				</Button>
-			</Card.Actions>
-		</Card>
-	);
+  return (
+    <Card style={theme.tripCard}>
+      <Card.Content>
+        <Title style={theme.tripTitle}>{tripDetails.name}</Title>
+        <Text variant="titleMedium">{tripDetails.date}</Text>
+        <Subheading style={{ color: "blue" }}>
+          {tripDetails.location}
+        </Subheading>
+        <Text variant="bodyMedium" style={theme.tripDesc}>
+          {tripDetails.description}
+        </Text>
+      </Card.Content>
+      <Card.Cover
+        style={{ padding: 6 }}
+        source={{ uri: "https://picsum.photos/700" }}
+      />
+      <Card.Actions
+        style={{ display: "flex", justifyContent: "space-between" }}
+      >
+        <Button
+          style={theme.tripTeacherButtons}
+          onPress={() =>
+            router.push({
+              pathname: `/trip/(${userRole})/[tripId]`,
+              params: {
+                tripId,
+                tripTitle: tripDetails.name,
+              },
+            })
+          }
+        >
+          <Text style={theme.buttonText}>Info</Text>
+        </Button>
+      </Card.Actions>
+    </Card>
+  );
 };
 
 export default GuardianTripCard;
--- src/components/triplist/TeacherTripCard.tsx
+++ src/components/triplist/TeacherTripCard.tsx
@@ -1,89 +1,102 @@
-import React, { useState } from 'react';
-import { Card, Button, Text, Title, Subheading } from 'react-native-paper';
-import TripEditForm from './EditTripForm';
-import { router } from 'expo-router';
+import React, { useState } from "react";
+import { Card, Button, Text, Title, Subheading } from "react-native-paper";
+import TripEditForm from "./EditTripForm";
+import { router } from "expo-router";
+import theme from "@/utils/theme";
 
 const TeacherTripCard = ({
-	tripId,
-	tripDetails,
-	handleEditTrip,
-	handleDeleteTrip,
-	userRole,
+  tripId,
+  tripDetails,
+  handleEditTrip,
+  handleDeleteTrip,
+  userRole,
 }) => {
-	const [isEditing, setIsEditing] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
 
-	const toggleEditForm = () => {
-		setIsEditing(!isEditing);
-	};
+  const toggleEditForm = () => {
+    setIsEditing(!isEditing);
+  };
 
-	const handleSaveEdit = (editedTrip) => {
-		handleEditTrip(editedTrip);
-		setIsEditing(false);
-	};
+  const handleSaveEdit = (editedTrip) => {
+    handleEditTrip(editedTrip);
+    setIsEditing(false);
+  };
 
-	return (
-		<Card style={{ margin: 16 }}>
-			<Button
-				icon="delete"
-				onPress={() => handleDeleteTrip(tripId)}
-				style={{
-					position: 'absolute',
-					top: 0,
-					right: 0,
-					zIndex: 1,
-					margin: 1,
-				}}
-			/>
-			<Card.Content>
-				<Title style={{ fontSize: 24, fontWeight: 'bold' }}>
-					{tripDetails.name}
-				</Title>
-				<Text variant="titleMedium">{tripDetails.date}</Text>
-				<Subheading style={{ color: 'blue' }}>
-					{tripDetails.location}
-				</Subheading>
-				<Text variant="bodyMedium">{tripDetails.description}</Text>
-			</Card.Content>
-			<Card.Cover
-				style={{ padding: 6 }}
-				source={{ uri: 'https://picsum.photos/700' }}
-			/>
-			<Card.Actions
-				style={{ display: 'flex', justifyContent: 'space-between' }}
-			>
-				<Button
-					mode="contained"
-					style={{ marginRight: 'auto', flex: 1 }}
-					onPress={() =>
-						router.push({
-							pathname: `/trip/(${userRole})/[tripId]`,
-							params: {
-								tripId,
-								tripTitle: tripDetails.name,
-							},
-						})
-					}
-				>
-					Start
-				</Button>
-				<Button
-					mode="contained"
-					style={{ marginLeft: 'auto', flex: 1 }}
-					onPress={toggleEditForm}
-				>
-					Edit
-				</Button>
-			</Card.Actions>
+  return (
+    <Card style={theme.tripCard}>
+      <Card.Content>
+        <Title style={theme.tripTitle}>{tripDetails.name}</Title>
+        <Text variant="titleMedium">{tripDetails.date}</Text>
+        <Subheading style={{ color: "blue" }}>
+          {tripDetails.location}
+        </Subheading>
+        <Text variant="bodyMedium" style={theme.tripDesc}>
+          {tripDetails.description}
+        </Text>
+      </Card.Content>
+      <Card.Cover
+        style={{ padding: 6 }}
+        source={{ uri: "https://picsum.photos/700" }}
+      />
+      <Card.Actions
+        style={{ display: "flex", justifyContent: "space-between" }}
+      >
+        <Button
+          icon="play"
+          mode="contained"
+          style={theme.tripTeacherButtons}
+          onPress={() =>
+            router.push({
+              pathname: `/trip/(${userRole})/[tripId]`,
+              params: {
+                tripId,
+                tripTitle: tripDetails.name,
+              },
+            })
+          }
+        >
+          <Text style={{ fontFamily: "Poetsen One", color: "white" }}>
+            Start
+          </Text>
+        </Button>
+        <Button
+          icon="clipboard-edit"
+          mode="contained"
+          style={theme.tripTeacherButtons}
+          onPress={toggleEditForm}
+        >
+          <Text style={{ fontFamily: "Poetsen One", color: "white" }}>
+            Edit
+          </Text>
+        </Button>
+        <Button
+          icon="delete"
+          mode="contained"
+          onPress={() => handleDeleteTrip(tripId)}
+          style={theme.tripDeleteButton}
+        >
+          <Text
+            style={{
+              color: "white",
+              fontFamily: "Poetsen One",
+              padding: 0,
+              fontSize: 16,
+            }}
+          >
+            Delete
+          </Text>
+        </Button>
+      </Card.Actions>
 
-			{isEditing && (
-				<TripEditForm
-					trip={tripDetails} // Pass tripDetails instead of trip
-					onSubmit={handleSaveEdit}
-					onCancel={toggleEditForm}
-				/>
-			)}
-		</Card>
-	);
+      {isEditing && (
+        <TripEditForm
+          trip={tripDetails} // Pass tripDetails instead of trip
+          onSubmit={handleSaveEdit}
+          onCancel={toggleEditForm}
+        />
+      )}
+    </Card>
+  );
 };
 
 export default TeacherTripCard;
